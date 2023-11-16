90c12817ec590e9d4702d6f3a12560fc2c358cc5
Yogui <972081300@qq.com>
2023-11-16 19:49:47 +0100

    Update NetworkMessage.cs

--- Assets/Scripts/Game/NetworkMessage.cs
+++ Assets/Scripts/Game/NetworkMessage.cs
@@ -34,9 +34,9 @@ public class NetworkPackage
 
         NetworkPackage networkPackage = JsonUtility.FromJson<NetworkPackage>(test);
 
-        var buffer = Encoding.ASCII.GetBytes(test);
+        //var buffer = Encoding.ASCII.GetBytes(test);
 
-        NetworkMessage message = NetworkPackage.GetDataFromBytes(buffer, buffer.Length);
+        //NetworkMessage message = NetworkPackage.GetDataFromBytes(buffer, buffer.Length);
 
         return Encoding.ASCII.GetBytes(JsonUtility.ToJson(this));
     }
